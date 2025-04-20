9b510dcc6a5a60b13131be5a61d9ac31b041babc
John Doe <johndoe@example.com>
2025-04-21 05:21:53 +0900

    vite.config.js修正

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/english_app_for_japanese/',
   plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  },
+  assetsInclude: ['**/*.wasm']
 })
